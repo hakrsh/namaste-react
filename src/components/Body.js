b8956f79ba525bd082460a2ce088042a4a4c0edd
Hari <harikn77@gmail.com>
2024-05-01 16:10:10 +0530

    Add search

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,25 +3,52 @@ import Shimmer from "./Shimmer";
 import { useState, useEffect } from "react";
 
 const Body = () => {
+  // Whenever state variables updates, react trigger a reconcilation cycle(re-render the component)
+  console.log("Body Rendered!");
   const [listOfProducts, setListOfProducts] = useState([]); // array destructuring
+  const [listOfFilteredProdcuts, setListOfFilteredProdcuts] = useState([]);
+  const [searchText, setSearchText] = useState("");
   useEffect(() => {
-    fetchData()
+    fetchData();
   }, []);
   const fetchData = async () => {
+    console.log("fetching data");
     const res = await fetch("https://dummyjson.com/products");
-    const data = await res.json()
-    setListOfProducts(data?.products)
+    const data = await res.json();
+    setListOfProducts(data?.products);
+    setListOfFilteredProdcuts(data?.products);
   };
   // Conditional rendering
-  return !listOfProducts.length ? <Shimmer/> : (
+  return !listOfProducts.length ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="search">
+        <div>
+          <input
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          ></input>
+          <button
+            onClick={() => {
+              setListOfFilteredProdcuts(
+                listOfProducts.filter((product) =>
+                  product.title.toLowerCase().includes(searchText.toLowerCase())
+                )
+              );
+            }}
+          >
+            Search
+          </button>
+        </div>
         <button
+          className="filter"
           onClick={() =>
-            setListOfProducts(
-              listOfProducts.filter(
-                (product) => product.rating > 4.5
-              )
+            setListOfFilteredProdcuts(
+              listOfFilteredProdcuts.filter((product) => product.rating > 4.5)
             )
           }
         >
@@ -29,7 +56,7 @@ const Body = () => {
         </button>
       </div>
       <div className="res-container">
-        {listOfProducts.map((product) => (
+        {listOfFilteredProdcuts.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
